51aeb8d6c8a1679ec63d6fe0287edae355ae8d24
Sonu bharti <sonukumarbodh01@gmail.com>
2026-01-27 23:47:34 +0530

    again update code

--- src/index.js
+++ src/index.js
@@ -7,17 +7,16 @@ import dotenv from "dotenv";
 dotenv.config({
   path: "src/.env",
 });
-connectDB();
-
-.then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log(`Server is, running at port : ${process.env.PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    app.listen();
+    console.error("Mongodb connection failed !!! ", error);
   });
-})
-.catch((error) => {
-  app.listen();
-  console.error("Mongodb connection failed !!! ", error);
-});
 
 /*
 (async () => {
